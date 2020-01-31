20ebc310678189af0c0b8d6d6e851e78c579bfcd
lordoscarr <paroscarandersson@live.com>
2020-01-31 09:44:44 +0100

    fix info and layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,84 +19,85 @@ export default function Layout(props) {
         <meta name="description" content={siteMetadata.description} />
       </Helmet>
       <header className="navbar"></header>
-      <main className="flex items-center flex-col">
-        <Sidebar
-          sidebar={
-            <>
-              <ul>
-                <li
-                  key="index"
-                  className={
-                    "text-2xl font-bold hover:underline cursor-pointer color-c " +
-                    (props.page === "index" && "underline")
-                  }
-                >
-                  <Link to="/">Start</Link>
-                </li>
-                <li
-                  key="menu"
-                  className={
-                    "text-2xl font-bold hover:underline cursor-pointer color-c " +
-                    (props.page === "menu" && "underline")
-                  }
-                >
-                  <Link to="/menu">Veckans meny</Link>
-                </li>
-                <li
-                  key="info"
-                  className={
-                    "text-2xl font-bold hover:underline cursor-pointer color-c " +
-                    (props.page === "info" && "underline")
-                  }
-                >
-                  <Link to="/info">Om oss</Link>
-                </li>
-              </ul>
-              <p className="absolute logo-font text-2xl color-b bottom-0 my-8">
-                Levarans
-              </p>
-            </>
-          }
-          open={sidebarOpen}
-          onSetOpen={() => {
-            console.log("sidebar open")
-          }}
-          styles={{
-            sidebar: {
-              background: "white",
-              paddingLeft: "1rem",
-              paddingRight: "10rem",
-              paddingTop: "10rem",
-            },
-          }}
-        />
-        <div
-          className={
-            "absolute top-0 left-0 my-2 mx-2 md:mx-4 lg:mx-4 md:my-12 lg:my-12 z-50 cursor-pointer"
-          }
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-        >
+      <Sidebar
+        sidebar={
+          <>
+            <ul>
+              <li
+                key="index"
+                className={
+                  "text-2xl font-bold hover:underline cursor-pointer color-c " +
+                  (props.page === "index" && "underline")
+                }
+              >
+                <Link to="/">Start</Link>
+              </li>
+              <li
+                key="menu"
+                className={
+                  "text-2xl font-bold hover:underline cursor-pointer color-c " +
+                  (props.page === "menu" && "underline")
+                }
+              >
+                <Link to="/menu">Veckans meny</Link>
+              </li>
+              <li
+                key="info"
+                className={
+                  "text-2xl font-bold hover:underline cursor-pointer color-c " +
+                  (props.page === "info" && "underline")
+                }
+              >
+                <Link to="/info">Om oss</Link>
+              </li>
+            </ul>
+            <p className="absolute logo-font text-2xl color-b bottom-0 my-8">
+              Levarans
+            </p>
+          </>
+        }
+        open={sidebarOpen}
+        onSetOpen={() => {
+          console.log("sidebar open")
+        }}
+        styles={{
+          sidebar: {
+            background: "white",
+            paddingLeft: "1rem",
+            paddingRight: "10rem",
+            paddingTop: "10rem",
+          },
+        }}
+      >
+        <main className="flex items-center flex-col bg-color-a">
           <div
             className={
-              "color-b p-2 rounded-full " +
-              (sidebarOpen ? "bg-color-a" : "bg-white")
+              "absolute top-0 left-0 my-2 mx-2 md:mx-4 lg:mx-4 md:my-12 lg:my-12 z-50 cursor-pointer"
             }
+            onClick={() => setSidebarOpen(!sidebarOpen)}
           >
-            <MdRestaurantMenu
-              lassName="my-8 mx-auto"
-              size="2rem"
-              color={sidebarOpen ? "white" : "#4d6b96"}
-            />
-            {/* <Burger isOpen={sidebarOpen} /> */}
+            <div
+              className={
+                "color-b p-2 rounded-full " +
+                (sidebarOpen ? "bg-color-a" : "bg-white")
+              }
+            >
+              <MdRestaurantMenu
+                lassName="my-8 mx-auto"
+                size="2rem"
+                color={sidebarOpen ? "white" : "#4d6b96"}
+              />
+              {/* <Burger isOpen={sidebarOpen} /> */}
+            </div>
           </div>
-        </div>
-        <div className="container w-11/12 md:w-2/3 lg:w-5/12 my-16">
-          <Header />
-          <div className="form-container bg-white rounded-lg p-6 my-16 shadow-xl">
-            <div>{props.children}</div>
+          <div className="container w-11/12 md:w-2/3 lg:w-5/12 my-16">
+            <Header />
+            <div className="form-container bg-white rounded-lg p-6 my-16 shadow-xl">
+              <div>{props.children}</div>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </Sidebar>
     </>
   )
 }
--- src/pages/info.js
+++ src/pages/info.js
@@ -2,12 +2,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import useSiteMetaData from "../static_queries/useSiteMetadata"
 import ReactMarkdown from "react-markdown"
+import "./info.scss"
 
 export default function Info() {
   const { infoData } = useSiteMetaData()
   return (
     <Layout page="info">
-      <ReactMarkdown source={infoData.about} />
+      <ReactMarkdown className="markdown" source={infoData.about} />
     </Layout>
   )
 }
